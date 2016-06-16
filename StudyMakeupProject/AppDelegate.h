31fad188c12bf676100da1da44fb52cad9868cff
Denis Boiarinoff <denis.boiarinoff@yandex.ru>
2016-06-16 09:57:53 +0600

    First commit

---
+++ StudyMakeupProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StudyMakeupProject
+//
+//  Created by Rhinoda3 on 16.06.16.
+//  Copyright © 2016 Rhinoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StudyMakeupProject/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  StudyMakeupProject
+//
+//  Created by Rhinoda3 on 16.06.16.
+//  Copyright © 2016 Rhinoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
